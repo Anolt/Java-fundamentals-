f65b4c0deee206c9988809f0b193df379fd0d0ed
Tarun Bansla <tbainsla5@gmail.com>
2026-06-21 00:49:17 +0530

    Understand theory of 'if else statements' and Do questions in the Question branch.

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
-// Program of taking user input .
 import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
+//        1. SCANNER CLASS - PROGRAM OF TAKING USER INPUT.
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.print("Hello , " + name );
+//        System.out.print("Enter your name: ");
+//        String name = scanner.nextLine();
+//        System.out.print("Hello , " + name );
+
+//        2. IF ELSE STATEMENTS -  Perform block of code if condition is true .
+
+//
         scanner.close();
     }
 }
